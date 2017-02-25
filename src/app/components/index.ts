16aaf2f5fa85db2fe10ee8da9e6b4cbc571863eb
Asim Hussain <jawache@gmail.com>
2017-02-25 19:24:11 +0000

    Migrated card component

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,3 +2,4 @@ import "./person-create.component";
 import "./person-edit.component";
 import "./person-list.component";
 import "./search.component";
+import "./card.component";
--- src/app/directives/index.ts
+++ src/app/directives/index.ts
@@ -1,2 +1,2 @@
-import "./card.directive";
+// import "./card.directive";
 import "./spinner.directive";
